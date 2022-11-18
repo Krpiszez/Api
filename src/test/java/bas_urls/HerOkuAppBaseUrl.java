2e288b98329752e7274ec20a7e5debe4bb0831a5
Omer Isik <isikomer4545@gmail.com>
2022-11-18 19:54:40 +0100

    third

---
+++ src/test/java/bas_urls/HerOkuAppBaseUrl.java
@@ -0,0 +1,17 @@
+package bas_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class HerOkuAppBaseUrl {
+
+    protected RequestSpecification spec;
+
+    @Before//If you use @Before annotation at the top of the method, it will work just before every test method.
+    public void setUp() {
+
+        spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com/").build();
+
+    }
+}
---
+++ src/test/java/get_request/Get05.java
@@ -0,0 +1,46 @@
+package get_request;
+
+import bas_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Get05 extends HerOkuAppBaseUrl {
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking
+        When
+            User sends get request to the URL
+        Then
+            Status code is 200
+	  	And
+	  		Among the data there should be someone whose firstname is "Agustin" and lastname is "Bonfanti"
+     */
+
+    @Test
+    public void get05(){
+        //Set the url
+        spec.pathParam("first", "booking").
+                queryParams("firstname","Agustin","lastname","Bonfanti");
+
+        //Set the expected Data
+
+
+        //Send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+
+        //Do Assertion
+
+        //Status code is 200
+        assertEquals(200,response.statusCode());
+
+        //Among the data there should be someone whose firstname is "Agustin" and lastname is "Bonfanti"
+        assertTrue(response.asString().contains("bookingid"));
+
+    }
+}
