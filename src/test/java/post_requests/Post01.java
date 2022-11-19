6faffe5716352cd0b3100f57d6809a38e2b54982
Omer Isik <isikomer4545@gmail.com>
2022-11-19 18:51:59 +0100

    second

---
+++ src/test/java/post_requests/Post01.java
@@ -0,0 +1,52 @@
+package post_requests;
+
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class Post01 extends JsonPlaceHolderBaseUrl {
+    /*
+    Given
+           1) https://jsonplaceholder.typicode.com/todos
+           2)  {
+                 "userId": 55,
+                 "title": "Tidy your room",
+                 "completed": false
+              }
+        When
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
+
+    @Test
+    public void post01(){
+
+        //Set the URL
+        spec.pathParam("first", "todos");
+
+        //Set the expected data
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("userId", 55);
+        expectedData.put("title", "Tidy your room");
+        expectedData.put("completed", false);
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request and get the response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
+        response.prettyPrint();
+    }
+}
